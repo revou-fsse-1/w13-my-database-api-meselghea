698279e73b99918cd59d2d823dd200f932355d5c
meselghea <122159476+meselghea@users.noreply.github.com>
2023-05-06 13:17:22 +0700

    update

--- prisma/migrations/20230506011635_product/migration.sql
+++ prisma/migrations/20230506011635_product/migration.sql
@@ -1,21 +1,27 @@
--- CreateTable
-CREATE TABLE "Seller" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
+BEGIN;
 
-    CONSTRAINT "Seller_pkey" PRIMARY KEY ("id")
+
+CREATE TABLE IF NOT EXISTS public."Product"
+(
+    id integer NOT NULL DEFAULT nextval('"Product_id_seq"'::regclass),
+    name character(50) COLLATE pg_catalog."default" NOT NULL,
+    price money NOT NULL,
+    quantity integer NOT NULL,
+    "sellerId" integer NOT NULL,
+    CONSTRAINT "Product_pkey" PRIMARY KEY (id)
 );
 
--- CreateTable
-CREATE TABLE "Product" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
-    "quantity" INTEGER NOT NULL,
-    "sellerId" INTEGER NOT NULL,
-
-    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+CREATE TABLE IF NOT EXISTS public."Seller"
+(
+    id integer NOT NULL DEFAULT nextval('"Seller_id_seq"'::regclass),
+    name character(50) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "Seller_pkey" PRIMARY KEY (id)
 );
 
--- AddForeignKey
-ALTER TABLE "Product" ADD CONSTRAINT "Product_sellerId_fkey" FOREIGN KEY ("sellerId") REFERENCES "Seller"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE IF EXISTS public."Product"
+    ADD CONSTRAINT "Product_sellerId_fkey" FOREIGN KEY ("sellerId")
+    REFERENCES public."Seller" (id) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT;
+
+END;
